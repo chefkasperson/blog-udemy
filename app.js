d92e28c0e543c060641d8c608bb0a29e41cf2a5a
chefkasperson <chefkasperson@gmail.com>
2020-07-26 06:23:56 -0700

    added contact and about routes

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(express.static("public"));
 app.get('/', function(req, res) {
   res.render("home", {startingContent: homeStartingContent})
 })
+app.get('/about', function(req, res) {
+  res.render("about", {startingContent: aboutContent})
+})
+app.get('/contact', function(req, res) {
+  res.render("contact", {startingContent: contactContent})
+})
 
 
 
