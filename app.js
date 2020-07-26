ad351c2856492f77b3aa24e3862c6610d007f52c
chefkasperson <chefkasperson@gmail.com>
2020-07-26 06:32:15 -0700

    updated compose page

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.get('/about', function(req, res) {
 app.get('/contact', function(req, res) {
   res.render("contact", {startingContent: contactContent})
 })
+app.get('/compose', function(req, res) {
+  res.render("compose")
+})
 
 
 
